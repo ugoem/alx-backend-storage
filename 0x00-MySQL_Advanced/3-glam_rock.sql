14c386d9eccb8055d363312cfcaaf54835c577e6
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2023-07-16 10:35:32 +0100

    create files advanced MySQL

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
